d9f6b38f9824a4c7e9e42ea0a7c3d2e4fda1d565
Autumn <barnes06.austin@gmail.com>
2023-04-01 11:15:02 -0700

    Made #include checking more accurately represent TD

--- Teardown Environment Addon/plugin.lua
+++ Teardown Environment Addon/plugin.lua
@@ -1,11 +1,17 @@
 function OnSetText(uri, text)
-    local diffs = {{start=1,finish=0,text="\n"}} -- Avoid the first line not getting the right syntax highlight
-    for startp, _, file, endp in ("\n" .. text):gmatch("\n()#include *([\"'])([^\r\n]+)%2()") do
+    local diffs = {}
+    -- local diffs = { { start = 1, finish = 0, text = "" } } -- Avoid the first line not getting the right syntax highlight
+
+    for start, _, file, finish in ("\n" .. text):gmatch("\n()#include ([\"']?)([^\r\n]*)%2()") do
+        -- local path = file:gsub("%.lua$", "")
+        
         diffs[#diffs + 1] = {
-            start = startp - 1,
-            finish = endp - 2,
-            text = string.format("---@module \"%s\"", file:gsub("%.lua$", ""))
+            start = start - 1,
+            finish = finish - 1,
+            text = string.format("---Including File \"%s\"", file)
         }
+
+        -- #include \"%s\"
     end
     return diffs
 end
